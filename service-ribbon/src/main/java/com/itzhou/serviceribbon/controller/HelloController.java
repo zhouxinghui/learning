d31b130294ab60d46180d446079d2dc31495d5f6
zhouxinghui <zhouxinghui@gold-gold.cn>
2018-07-23 11:04:07 +0800

    first commit

---
+++ service-ribbon/src/main/java/com/itzhou/serviceribbon/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.itzhou.serviceribbon.controller;
+
+import com.itzhou.serviceribbon.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhouxinghui
+ * @create 2018/7/23
+ */
+@RestController
+public class HelloController {
+    @Autowired
+    HelloService helloService;
+    @RequestMapping(value = "/hi")
+    public String hi(@RequestParam String name){
+        return helloService.hiService(name);
+    }
+}
---
+++ service-ribbon/src/main/java/com/itzhou/serviceribbon/service/HelloService.java
@@ -0,0 +1,19 @@
+package com.itzhou.serviceribbon.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author zhouxinghui
+ * @create 2018/7/23
+ */
+@Service
+public class HelloService {
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+}
