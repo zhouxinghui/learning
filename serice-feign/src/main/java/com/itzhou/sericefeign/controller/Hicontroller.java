298b6a48b02f57c488d733c881b6cbd086d3de9d
zhouxinghui <zhouxinghui@gold-gold.cn>
2018-07-23 11:30:21 +0800

    second commit

---
+++ serice-feign/src/main/java/com/itzhou/sericefeign/controller/Hicontroller.java
@@ -0,0 +1,22 @@
+package com.itzhou.sericefeign.controller;
+
+import com.itzhou.sericefeign.feignclient.SchedualServiceHi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhouxinghui
+ * @create 2018/7/23
+ */
+@RestController
+public class Hicontroller {
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    public String sayHi(@RequestParam String name){
+        return schedualServiceHi.sayHiFromClientOne(name);
+    }
+}
---
+++ serice-feign/src/main/java/com/itzhou/sericefeign/feignclient/SchedualServiceHi.java
@@ -0,0 +1,16 @@
+package com.itzhou.sericefeign.feignclient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author zhouxinghui
+ * @create 2018/7/23
+ */
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
